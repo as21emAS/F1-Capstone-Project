0ae134216eee76cd65ce4316d0aa4f5285c4f783
BrooklynMetzger <root@Cheryl.localdomain>
2026-02-16 17:54:45 -0500

    Added basic dashborad and pages pathways

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -3,33 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	<Router>
+		<div className="min-h-screen bg-gray-900 text-white font-sans">
+		{/* Nav Bar */}
+		<nav className="p-4 bg-gray-800 border-b border-red-600 flex gap-6">
+	<Link to="/" className="hover:text-red-500">Dashboard</Link>
+	</nav>
+
+	<main className="p-8">
+		<Routes>
+			<Route path="/" element={<Dashboard />} />
+		</Routes>
+	      </main>
+	     </div>
+	    </Router>
+  );
 }
 
 export default App
