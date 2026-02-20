e1e976297372c05d01d11ff0a26370a82a7add13
BrooklynMetzger <root@Cheryl.localdomain>
2026-02-20 15:41:09 -0500

    Added issue 7#;  Tailwind CSS, custom f1 colors, path aliases, set up ESLint and Prettier

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,31 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+/*import './App.css';*/
+import Dashboard from '@pages/Dashboard';
+/*import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';*/
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-	<Router>
-		<div className="min-h-screen bg-gray-900 text-white font-sans">
-		{/* Nav Bar */}
-		<nav className="p-4 bg-gray-800 border-b border-red-600 flex gap-6">
-	<Link to="/" className="hover:text-red-500">Dashboard</Link>
-	</nav>
-
-	<main className="p-8">
-		<Routes>
-			<Route path="/" element={<Dashboard />} />
-		</Routes>
-	      </main>
-	     </div>
-	    </Router>
+    <Dashboard />
   );
 }
 
-export default App
+export default App;
--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,17 +1,14 @@
 export default function Dashboard() {
+  /* testing .env */
+  console.log("Jolpica URL:", import.meta.env.VITE_JOLPICA_BASE_URL);
   return (
-    <div className="space-y-6">
-      <h1 className="text-3xl font-bold border-l-4 border-red-600 pl-4">Race Dashboard</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <h2 className="text-xl font-semibold mb-4">Next Race Prediction</h2>
-          <p className="text-gray-400">Real-time ML prediction goes here...</p>
-        </div>
-        <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-          <h2 className="text-xl font-semibold mb-4">Driver Standings</h2>
-          <p className="text-gray-400">Current season ranking leaderboard...</p>
-        </div>
-      </div>
+    <div className="flex min-h-screen w-full items-center justify-center bg-f1-black m-0">
+      <h1 className="font-racing text-5xl text-f1-red">
+      	Welcome to the F1 Dashboard
+      </h1>
+      <h2 className="font-inter text-3xl text-redbull-blue ml-8">
+      	This is going to be great. Just you wait
+      </h2>
     </div>
   );
 }
